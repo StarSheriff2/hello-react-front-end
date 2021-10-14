0a98310911590e0f9b6414920fb26e9de654b412
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-13 22:47:43 -0600

    add Logger to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import greetingReducer from '../features/greeting/greetingSlice';
 
 const store = configureStore({
   reducer: {
     greeting: greetingReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
